f2e2dc30a000424a5687b68c1cd9891a809d4b6e
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-01-22 01:22:39 +0200

    Changed usages in main

--- main.py
+++ main.py
@@ -4,22 +4,24 @@ Main script for ASManifest
 from argparse import ArgumentParser, FileType
 from typing import IO
 
+from src.Args import Args
 from src.Compiler import Compiler
 
 print("\n[ ASManifest Compiler ]\n")
 
-# Get arguments
+# Get arguments with argument parser class
 parser = ArgumentParser()
-# Add args
+# Add args to the parser
 parser.add_argument('file', type=FileType(), help='file to compile')
 parser.add_argument('-v', '--verbose', action='store_true', help='print verbose compilation steps')
 parser.add_argument('-c', '--comment', action='store_true', help='adds verbose comments to the output file')
 parser.add_argument('-o', '--output', type=FileType('w'), help='the file to output the ASM code to')
-# Parse args
-args = parser.parse_args()
+# Parse args, then create
+# a singleton from arguments
+Args(parser.parse_args())
 
 # Get the source file
-ioStream: IO = args.file
+ioStream: IO = Args().get_args().file
 # Read the source file
 print("Reading source file...")
 source = ioStream.read()
@@ -28,7 +30,7 @@ ioStream.close()
 
 # Send parser data to compiler
 print("Parsing the file...")
-c: Compiler = Compiler(args)
+c: Compiler = Compiler(Args().get_args())
 
 # Compile the file
 c.parse(source)
@@ -36,7 +38,7 @@ print("Successfully parsed!")
 
 # If there is an output file, then write there
 # Otherwise, add .cpp to the file and write there
-ioStream = args.output if args.output else open(args.file + '.cpp', "w")
+ioStream = Args().get_args().output if Args().get_args().output else open(Args().get_args().file + '.cpp', "w")
 
 # Compile the file to a string
 print("Compiling the file...")
