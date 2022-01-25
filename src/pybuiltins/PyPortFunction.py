7c6d32fbc45cd5f9634895a7f618860cf6050a8f
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-01-25 10:26:52 +0200

    Moved PyPortFunction to use PyExpression instead of PyPort

--- src/pybuiltins/PyPortFunction.py
+++ src/pybuiltins/PyPortFunction.py
@@ -1,13 +1,13 @@
 """
 Port a native function or object to Python.
 """
-from typing import Any, Iterable, Optional
+from typing import Any, Iterable, Optional, Set
 
-from src.pybuiltins.PyPort import PyPort
+from src.pyexpressions.PyExpression import PyExpression
 from src.structures.TypeRenames import AnyFunction
 
 
-class PyPortFunction(PyPort):
+class PyPortFunction(PyExpression):
 	"""
 	Port a native function or object to Python.
 	"""
@@ -16,22 +16,38 @@ class PyPortFunction(PyPort):
 	# can't specify it here without a circular import
 	# error so we will (overwrite) type hint in the constructor instead.
 	__func: Any
+	# Native code as a string
 	__code: str
+	# A list of native dependencies
+	__native_depends: Set[str]
 
 	def __init__(self, function: AnyFunction, code: str, dependencies: Optional[Iterable[str]] = None):
 		# Create a new set if there are no dependencies passed, otherwise use the passed dependencies
-		super().__init__(set() if dependencies is None else set(dependencies))
+		super().__init__(None, None)
 
 		# Import locally so that PyExpression can use this directly
 		from src.pyexpressions.PyFunctionDef import PyFunctionDef
-
 		# Convert the function to a PyFunctionDef that can be represented locally later (as function header)
 		self.__func: PyFunctionDef = PyFunctionDef.from_single_object(function, self)
-
 		# Store the native code segment
 		self.__code = code
 
-	def transpile(self) -> str:
+		# Initialize the dependency set
+		self.__native_depends = set()
+		# If there are any dependencies
+		if dependencies is not None:
+			# Add them as an iterable
+			self.add_native_dependencies(dependencies)
+
+	def add_native_dependencies(self, native_dependencies: Iterable[str]) -> None:
+		"""
+		Adds multiple native dependencies to the dependency list.
+
+		:param native_dependencies: A list of native dependencies that this object relies on.
+		"""
+		self.__native_depends.update(native_dependencies)
+
+	def _transpile(self) -> str:
 		"""
 		Transpile the ported function to an entirely native function.
 		"""
