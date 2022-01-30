13591c21ed302e2064308be9a4fa9ab20d5e59c1
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-01-30 21:18:42 +0200

    Fixed transpilation for elif/else

--- examples/conditional_print.cpp
+++ examples/conditional_print.cpp
@@ -1,14 +1,5 @@
 #include <iostream>
 void print(str print_string){std::cout<<print_string<<std::endl;}
 
-void conditional_print(int index){
-print(index);
-if () {
-print("Equals to 0!");
-}
-if () {
-print("Smaller than 3!");
-}
-print("Other...");
-conditional_print((index+1));}
+void conditional_print(int index){print(index);if () {print("Equals to 0!");}else if () {print("Smaller than 3!");}else {print("Other...");}conditional_print((index+1));}
 conditional_print(0);
--- src/pyexpressions/concrete/PyIf.py
+++ src/pyexpressions/concrete/PyIf.py
@@ -18,22 +18,27 @@ class PyIf(PyConditional):
 	__elif: Optional["PyIf"]
 	__else: Optional[PyExpression]
 
-	def __init__(self, expression: If, parent: GENERIC_PYEXPR_TYPE):
-		super().__init__(expression, "if", parent)
+	def __init__(self, expression: If, parent: GENERIC_PYEXPR_TYPE, if_type: str = "if"):
+		super().__init__(expression, if_type, parent)
 		# If there is an "or else"
-		orelse = Compiler.get_attr(expression, "orelse")[0]
-		# Check that this is an if statement
-		if isinstance(orelse, If):
-			# Send to "if else"
-			self.__elif = PyIf(orelse, self)
-			self.__else = None
-		else:
-			# Send to "else"
-			self.__elif = None
-			self.__else = self.from_ast(orelse)
+		orelse_list = Compiler.get_attr(expression, "orelse")
+		if orelse_list:
+			# Get expression
+			orelse = orelse_list[0]
+			# If this is an "If" expression, then this is meant to be an "elif" statement
+			if isinstance(orelse, If):
+				# Send to "if else"
+				self.__elif = PyIf(orelse, self, if_type="else if")
+				self.__else = None
+			else:
+				# Send to "else"
+				self.__elif = None
+				self.__else = self.from_ast(orelse)
 
 	def _transpile(self) -> str:
 		"""
 		Transpile the conditional statement to a string.
 		"""
-		return super()._transpile() + self.__elif.transpile() if self.__elif is not None else self.__else.transpile()
+		return super()._transpile() + \
+			self.__elif.transpile() if self.__elif is not None else \
+			f"else {{{self.__else.transpile()}}}"
