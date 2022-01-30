24475d274ed235fb2656b9937c55534b23c97c31
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-01-30 20:26:19 +0200

    Tried to implement else/elif

--- examples/conditional_print.cpp
+++ examples/conditional_print.cpp
@@ -2,10 +2,13 @@
 void print(str print_string){std::cout<<print_string<<std::endl;}
 
 void conditional_print(int index){
-	print(index);
-	if () {
-		print("Equals to 0!");
-	}
-	conditional_print((index+1));
+print(index);
+if () {
+print("Equals to 0!");
 }
+if () {
+print("Smaller than 3!");
+}
+print("Other...");
+conditional_print((index+1));}
 conditional_print(0);
--- src/pyexpressions/concrete/PyIf.py
+++ src/pyexpressions/concrete/PyIf.py
@@ -1,9 +1,12 @@
 """
 Class for a conditional statement.
 """
-from _ast import IfExp
+from _ast import If
+from typing import Optional
 
+from src.compiler.Compiler import Compiler
 from src.pyexpressions.abstract.PyConditional import PyConditional
+from src.pyexpressions.abstract.PyExpression import PyExpression
 from src.structures.TypeRenames import GENERIC_PYEXPR_TYPE
 
 
@@ -12,5 +15,25 @@ class PyIf(PyConditional):
 	Class for a Python conditional statement.
 	"""
 
-	def __init__(self, expression: IfExp, parent: GENERIC_PYEXPR_TYPE):
+	__elif: Optional["PyIf"]
+	__else: Optional[PyExpression]
+
+	def __init__(self, expression: If, parent: GENERIC_PYEXPR_TYPE):
 		super().__init__(expression, "if", parent)
+		# If there is an "or else"
+		orelse = Compiler.get_attr(expression, "orelse")[0]
+		# Check that this is an if statement
+		if isinstance(orelse, If):
+			# Send to "if else"
+			self.__elif = PyIf(orelse, self)
+			self.__else = None
+		else:
+			# Send to "else"
+			self.__elif = None
+			self.__else = self.from_ast(orelse)
+
+	def _transpile(self) -> str:
+		"""
+		Transpile the conditional statement to a string.
+		"""
+		return super()._transpile() + self.__elif.transpile() if self.__elif is not None else self.__else.transpile()
