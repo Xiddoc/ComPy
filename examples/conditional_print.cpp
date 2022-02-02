68d14be665887b6335a803d5ad1383cab00f59c7
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-02-02 23:33:19 +0200

    PyCompare beautify

--- examples/conditional_print.cpp
+++ examples/conditional_print.cpp
@@ -3,9 +3,9 @@
 
 void print(str print_string){std::cout<<print_string<<std::endl;}
 
-void conditional_print(int index){print(index);if (((index==0&&index!=2)||index!=3)) {
+void conditional_print(int index){print(index);if (((index == 0 && index != 2) || index != 3)) {
 print("Equals to 0!");
-} else if (1<2<index<5) {
+} else if (1 < 2 < index < 5) {
 print("Larger than 2 and smaller than 5!");
 } else {
 print("Other...");print("(Did not match any other conditionals)");
