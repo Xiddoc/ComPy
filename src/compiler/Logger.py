24fb6516aec8bf845f8c7c486cc4d87a88e3338e
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-02-03 10:07:01 +0200

    Semi-implemented tabs

--- src/compiler/Logger.py
+++ src/compiler/Logger.py
@@ -15,13 +15,15 @@ class Logger:
 	is passed between to each classes.
 	"""
 
-	__indentation: int
+	__log_indentation: int
+	__tab_indentation: int
 
 	def __init__(self, py_expr: PyExpression) -> None:
 		# Figure out indentation level
 		# We do this by figuring out how many parents there are to this node.
 		temp_expr: PyExpression = py_expr
 		indentation: int = 0
+		self.__tab_indentation = 0
 
 		# Keep iterating up the "parent node chain"
 		# PyPortFunction will throw an error as get_parent does not exist
@@ -30,13 +32,27 @@ class Logger:
 			# Cast parent to a PyExpression, since
 			# we have the guarantee that it is one
 			temp_parent: PyExpression = cast(PyExpression, temp_expr.get_parent())
+
+			# Check if it requires a real indent
+			# For each of the possible classes that could require an indent
+			from src.pyexpressions.concrete.PyIf import PyIf
+			from src.pyexpressions.concrete.PyModule import PyModule
+			from src.pyexpressions.concrete.PyFunctionDef import PyFunctionDef
+			for class_type in [PyIf, PyModule, PyFunctionDef]:
+				# If this parent is one of those
+				if isinstance(temp_parent, class_type):
+					# Increment the tab counter
+					self.__tab_indentation += 1
+					# Exit the loop, we are done here
+					break
+
 			# Increment the count
 			indentation += 1
 			# Iterate to next parent
 			temp_expr = temp_parent
 
 		# Set to field
-		self.__indentation = indentation
+		self.__log_indentation = indentation
 
 	def log_tree_up(self, message: str) -> None:
 		"""
@@ -50,7 +66,7 @@ class Logger:
 		# Log it
 		self.log(
 			self.__get_log_prepend(
-				indentation=self.__indentation,
+				indentation=self.__log_indentation,
 				point_upwards=True
 			) + message
 		)
@@ -67,7 +83,7 @@ class Logger:
 		# Log it
 		self.log(
 			self.__get_log_prepend(
-				indentation=self.__indentation,
+				indentation=self.__log_indentation,
 				point_upwards=False
 			) + message
 		)
@@ -92,15 +108,21 @@ class Logger:
 		:return: A string of unicode symbols, spaces, and newlines which forms one line of the tree.
 		"""
 		# If first layer of tree, then use T symbol
-		if self.__indentation == 1:
+		if self.__log_indentation == 1:
 			return "├── "
 		# If the indentation is any more than 1, then place root branch on first line
 		# Then, branch off of the previous node (hence, indentation - 1)
-		elif self.__indentation > 1:
-			return "│" + "\t" * (self.__indentation - 1) + ("└" if point_upwards else "┌") + "── "
+		elif self.__log_indentation > 1:
+			return "│" + "\t" * (self.__log_indentation - 1) + ("└" if point_upwards else "┌") + "── "
 		# Otherwise, if this is the root branch (layer zero)
 		# Make a newline to seperate from previous node tree.
 		return "\n"
+
+	def get_tab_indentation(self) -> int:
+		"""
+		Returns this Logger instance's tab indentation.
+		"""
+		return self.__tab_indentation
 
 	@staticmethod
 	def escape(string: str) -> str:
