4aa6cd9e354e690e15b0b7869705f02da9015d19
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-03-28 18:20:20 +0300

    Wrote test for classes

---
+++ examples/test_class.py
@@ -0,0 +1,14 @@
+"""
+Testing a class in Python.
+"""
+
+
+class TestClass:
+    """
+    Class documentation.
+    """
+
+    test_field: int
+
+    def __init__(self) -> None:
+        self.test_field = 1
