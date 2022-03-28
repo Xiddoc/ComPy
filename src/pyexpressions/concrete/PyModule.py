a998fa83fe3f904b5b3cd44642cbf5509ea031de
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-03-28 18:13:50 +0300

    Moved PyFunc and PyModule to PyScoped

--- src/pyexpressions/concrete/PyModule.py
+++ src/pyexpressions/concrete/PyModule.py
@@ -8,21 +8,18 @@ from src.pyexpressions.abstract.PyExpression import PyExpression
 from src.pyexpressions.concrete.PyExpr import PyExpr
 from src.pyexpressions.concrete.PyFunctionDef import PyFunctionDef
 from src.pyexpressions.concrete.PyPass import PyPass
-from src.scopes.Scope import Scope
+from src.pyexpressions.highlevel.PyScoped import PyScoped
 
 
-class PyModule(PyExpression):
+class PyModule(PyScoped):
 	"""
 	Python module.
 	"""
 
 	__body: List[PyExpression]
-	__scope: Scope
 
 	def __init__(self, expression: Module):
 		super().__init__(expression, None)
-		# Create empty object scope
-		self.__scope = Scope()
 		# For each body expression
 		# Create a PyExpression from the AST node
 		self.__body = [self.from_ast(ast) for ast in expression.body]
@@ -75,19 +72,3 @@ class PyModule(PyExpression):
 			# Join the transpiled code
 			transpiled_code="\n".join(output_list)
 		)
-
-	# noinspection PyUnusedFunction
-	def get_scope(self) -> Scope:
-		"""
-		Returns the Scope (instance) of this module body.
-
-		Might have a warning in your IDE that labels it as "unused".
-		This is since it is not explicitly used (in PyExpression:
-		it *should* be casted to PyFunctionDef, then use obj.get_scope(),
-		but instead there is a type check, then we use get_scope.
-
-		What this means is that depending on the Python linter
-		implementation, your IDE could flag this function as useless.
-		It is not. Do **NOT** remove it.
-		"""
-		return self.__scope
